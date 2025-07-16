0fc36feec9cfff5c76de73b8cbc8fe98231e08c4
Grissess <grissess@nexusg.org>
2025-07-16 12:03:23 -0400

    Also allow %seclabel for title_format
    This allows users to include security labels as an immediately-visible
    part of a window's title.

--- sway/tree/container.c
+++ sway/tree/container.c
@@ -764,6 +764,9 @@ size_t parse_title_format(struct sway_container *container, char *buffer) {
 			} else if (has_prefix(next, "%sandbox_instance_id")) {
 				len += append_prop(buffer, view_get_sandbox_instance_id(container->view));
 				format += strlen("%sandbox_instance_id");
+			} else if (has_prefix(next, "%seclabel")) {
+				len += append_prop(buffer, container->view->seclabel ? container->view->seclabel : "");
+				format += strlen("%seclabel");
 			} else {
 				lenient_strcat(buffer, "%");
 				++format;
